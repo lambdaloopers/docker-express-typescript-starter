c2d38b92abe53f44223b9b02df39ef374d202254
nhal <originhality@hotmail.com>
2018-08-30 17:25:59 +0200

    First commit, repository startup

---
+++ app/src/controllers/api.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+import { Request, Response } from "express";
+
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+export let getApi = (req: Request, res: Response) => {
+    res.render("api/index", {
+        title: "API Examples"
+    });
+};
